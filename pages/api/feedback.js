ae4cecd21bccaf1c90f5e47cf83ccae7a8b20526
dhruv50ae <linkdp2012@gmail.com>
2022-07-04 04:13:39 +0100

    Add API Code
    The data gathered from the front end should be sent to a backend though an API

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,7 +1,27 @@
+import fs from "fs";
+import path from "path";
+
 const handler = (req, res) => {
-  res.status(200).json({
-    message: "this works",
-  });
+  if (req.method === "POST") {
+    const email = req.body.email;
+    const text = req.body.text;
+    const newFeedback = {
+      email,
+      text,
+      id: new Date().toISOString(),
+    };
+
+    const filePath = path.join(process.pwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success!", feedback: newFeedback });
+  } else {
+    res.status(200).json({
+      message: "this works",
+    });
+  }
 };
 
 export default handler;
