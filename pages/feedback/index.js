523ace27d6c477a3aecfcd31b9f8f1e005ab7db6
dhruv50ae <linkdp2012@gmail.com>
2022-07-04 14:17:41 +0100

    Data fetching using dynamic api paths

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,28 @@
+import { useState } from "react";
 import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 const FeedbackPage = ({ feedbackItems }) => {
+  const [feedbackData, setFeedbackData] = useState();
+  const loadFeedbackHandler = (id) => {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => setFeedbackData(data.feedback));
+  };
+
   return (
-    <ul>
-      {feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 export async function getStaticProps() {
