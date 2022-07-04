5ba3e21a005f7cf60a546514f740026979484265
dhruv50ae <linkdp2012@gmail.com>
2022-07-04 03:59:05 +0100

    Create refs for data
    UseRef is useful here for saving data and routing it to an api so that it can be saved to a backend

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
+import { useRef } from "react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+  const submitFormHandler = (e) => {
+    e.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  };
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea
+            type="text"
+            id="feedback"
+            rows="5"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
+  );
 };
 
 export default HomePage;
